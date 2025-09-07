72cf1aee766282938c2249069209962dd24e38b0
LaxmiN801 <ajayKanimate@gmail.com>
2025-09-07 19:36:14 +0530

    fixed cors problem

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -5,7 +5,7 @@ import { io } from "socket.io-client";
 
 const BASE_URL = import.meta.env.MODE === "development" 
   ? "http://localhost:8000" 
-  : import.meta.env.VITE_BACKEND_URL; 
+  : import.meta.env.VITE_BACKEND_URL;
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
