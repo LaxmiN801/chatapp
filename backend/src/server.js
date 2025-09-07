6d9fc9b534d5d5b0489b6367efcc438a4626ded6
LaxmiN801 <ajayKanimate@gmail.com>
2025-09-07 17:57:37 +0530

    ready to deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,16 +4,11 @@ dotenv.config();
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import path from "path";
-
 import connectDB from "./config/db.js";
-
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import contactRoutes from "./routes/contact.route.js";
 import { app, server } from "./lib/socket.js";
-
-const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
@@ -26,31 +21,17 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/contacts", contactRoutes);
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-    app.get("/*", (req, res) => {
-        res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-    });
-}
-
 app.use((err, req, res, next) => {
     console.error("Unexpected error:", err.stack);
     res.status(500).json({ message: "Something broke" });
 });
 
-
-// server.listen(process.env.PORT || 5000, () => {
-//   console.log("server is running on PORT:" + process.env.PORT);
-//   connectDB();
-// });
-
 connectDB()
 	.then(() => {
 		server.listen(process.env.PORT || 5000, () => {
 			console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
-		})
+		});
 	})
 	.catch((err) => {
 		console.log(err);
-	})
+	});
